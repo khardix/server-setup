018bbae2c351cf656e13024540828f1a965af99d
Jan “Khardix” Staněk <khardix@gmail.com>
2019-07-23 21:55:45 +0200

    feat(website): Implement automatic language selection

--- roles/khardix.website/molecule/default/tests/test_default.py
+++ roles/khardix.website/molecule/default/tests/test_default.py
@@ -10,7 +10,7 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
 
 
-ROOT_DIR = Path("/srv/http/example.com")
+ROOT_DIR = Path("/srv/http/example.com/")
 
 
 @pytest.fixture(scope="module")
@@ -18,12 +18,24 @@ def index_file(host):
     """Create index file with known content."""
 
     content = ":".join(random.choices(ascii_letters, k=8))
-    target = ROOT_DIR / "index.html"
-    command = f"echo '{content}' >'{target}' && chmod 0644 '{target}'"
+    target = ROOT_DIR / "en" / "index.html"
+    command = f"mkdir -p '{target.parent}' && echo '{content}' >'{target}' && chmod 0644 '{target}'"
 
     host.run_expect({0}, command)
 
     return content
+
+
+@pytest.fixture()
+def curl():
+    """Base curl command with additional options"""
+
+    return [
+        "curl",
+        "--insecure",
+        "--resolve example.com:80:127.0.0.1",
+        "--resolve example.com:443:127.0.0.1",
+    ]
 
 
 def test_root_is_accessible(host):
@@ -36,17 +48,23 @@ def test_root_is_accessible(host):
     assert root.mode & S_IRWXU == S_IRWXU
 
 
-def test_server_serves_content(host, index_file):
+def test_server_redirects_to_expected_subdir(host, curl):
+    """Server automatically redirect from root"""
+
+    command = curl + ["--head", "https://example.com/"]
+    result = host.run(" ".join(command))
+
+    assert result.rc == 0
+
+    response = result.stdout.strip().splitlines()
+    assert "303" in response[0]  # HTTP_SEE_OTHER
+    assert "Location: /en/" in response[1:]  # redirect
+
+
+def test_server_serves_content(host, curl, index_file):
     """Server serves expected content"""
 
-    command = [
-        "curl",
-        "--insecure",
-        "--location",
-        "--resolve example.com:80:127.0.0.1",
-        "--resolve example.com:443:127.0.0.1",
-        "http://example.com",
-    ]
+    command = curl + ["--location", "http://example.com/en/"]
     result = host.run(" ".join(command))
 
     assert result.rc == 0
