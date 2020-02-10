056014a210d9f075f22697ebc6638a0ec1cf5265
Jan Staněk <jstanek@redhat.com>
2020-02-10 16:10:09 +0100

    khardix.server-ports: Fix expected repo name

--- roles/khardix.server-ports/molecule/default/tests/test_default.py
+++ roles/khardix.server-ports/molecule/default/tests/test_default.py
@@ -1,13 +1,18 @@
 import os
+import shlex
 
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
 
+SERVER_PORTS_COPR = "copr:copr.fedorainfracloud.org:jstanek:server-ports"
+
 
 def test_repository_enabled(host):
-    with host.sudo():
-        result = host.run("yum -q repo-pkgs jstanek-server-ports list")
+    command = ["yum", "-q", "repo-pkgs", SERVER_PORTS_COPR, "list"]
 
-    assert result.rc == 0
+    with host.sudo():
+        result = host.run(" ".join(map(shlex.quote, command)))
+
+    assert result.rc == 0, result.stderr
     assert len(result.stdout) != 0
