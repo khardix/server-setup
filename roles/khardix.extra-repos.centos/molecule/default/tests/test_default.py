a55134864fc6a5d009dc098d58f41ad58ecc5394
Jan Staněk <jstanek@redhat.com>
2020-02-10 17:17:27 +0100

    extra-repos.centos: Import epel installation

--- roles/khardix.extra-repos.centos/molecule/default/tests/test_default.py
+++ roles/khardix.extra-repos.centos/molecule/default/tests/test_default.py
@@ -1,15 +1,34 @@
 import os
+import shlex
+from types import MappingProxyType
 
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+YUM = ("yum", "-q")
+
+# Package name -> repository name
+REPOSITORY_PACKAGE_MAP = MappingProxyType({"epel-release": "epel"})
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def run_safe(host, command):
+    """Run command with quoted words"""
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    return host.run(" ".join(map(shlex.quote, command)))
+
+
+@pytest.mark.parametrize("package,repo", REPOSITORY_PACKAGE_MAP.items())
+def test_repo_from_rpm_is_enabled(host, package, repo):
+    """A repository installed from RPM package is enabled"""
+
+    list_installed = YUM + ("list", "installed", package)
+    installed = run_safe(host, list_installed)
+    assert installed.rc == 0 and package in installed.stdout, installed.stderr
+
+    list_enabled = YUM + ("repolist", "enabled", repo)
+    enabled = run_safe(host, list_enabled)
+    assert enabled.rc == 0 and repo in enabled.stdout, enabled.stderr
