1705924469c0e66d772b5294d03712548965372e
Jan Staněk <jstanek@redhat.com>
2020-02-12 14:48:22 +0100

    extra_repos.centos: Import server-ports

--- roles/khardix.extra-repos.centos/molecule/default/tests/test_default.py
+++ roles/khardix.extra-repos.centos/molecule/default/tests/test_default.py
@@ -13,6 +13,8 @@ YUM = ("yum", "-q")
 
 # Package name -> repository name
 REPOSITORY_PACKAGE_MAP = MappingProxyType({"epel-release": "epel"})
+# Installed COPRs
+COPR_LIST = ["copr:copr.fedorainfracloud.org:jstanek:server-ports"]
 
 
 def run_safe(host, command):
@@ -32,3 +34,12 @@ def test_repo_from_rpm_is_enabled(host, package, repo):
     list_enabled = YUM + ("repolist", "enabled", repo)
     enabled = run_safe(host, list_enabled)
     assert enabled.rc == 0 and repo in enabled.stdout, enabled.stderr
+
+
+@pytest.mark.parametrize("copr", COPR_LIST)
+def test_copr_is_enabled(host, copr):
+    """A COPR repository is enabled"""
+
+    list_enabled = YUM + ("repolist", "enabled", copr)
+    enabled = run_safe(host, list_enabled)
+    assert enabled.rc == 0 and copr in enabled.stdout, enabled.stderr
